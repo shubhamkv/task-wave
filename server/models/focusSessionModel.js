8ec6378a04026171cb20927f25b5d5aad29ad5bd
shubhamkv <shubh082002@gmail.com>
2025-04-28 22:59:30 +0530

    add focus session schema

--- server/models/focusSessionModel.js
+++ server/models/focusSessionModel.js
@@ -4,14 +4,22 @@ const focusSessionSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
   taskId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Task",
+    required: false,
   },
-  duration: Number,
-  startedAt: Date,
-  endedAt: Date,
+  taskName: {
+    type: String,
+    required: true,
+  },
+  duration: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now() },
+  startedAt: { type: Date, required: true },
+  endedAt: { type: Date, required: true },
+  completed: { type: Boolean, default: true },
 });
 
 const FocusSession = mongoose.model("FocusSession", focusSessionSchema);
