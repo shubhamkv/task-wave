b53b88640390c80e4990ba86488d7f8a55741d4b
shubhamkv <shubh082002@gmail.com>
2025-04-30 11:17:51 +0530

    add focus session page

---
+++ client/src/pages/FocusSession.jsx
@@ -0,0 +1,29 @@
+import { SessionHistory } from "../components/FocusSession/SessionHistory";
+import { TaskSelector } from "../components/FocusSession/TaskSelector";
+import { Timer } from "../components/FocusSession/Timer";
+import { SessionProvider } from "../context/SessionContext";
+
+export const FocusSession = () => {
+  return (
+    <SessionProvider>
+      <div className="p-4 md:p-8 bg-background text-foreground min-h-screen">
+        <div className="text-center space-y-2 mb-10">
+          <h1 className="text-3xl md:text-5xl font-bold">Focus Session</h1>
+          <p className="text-muted-foreground">
+            Stay focused, crush your goals! 🚀
+          </p>
+        </div>
+
+        <TaskSelector />
+
+        <div className="my-10">
+          <Timer />
+        </div>
+
+        <div className="my-10">
+          <SessionHistory />
+        </div>
+      </div>
+    </SessionProvider>
+  );
+};
