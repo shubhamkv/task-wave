79f0285ccd019da3a18d9cca6486f69db9fad39c
shubhamkv <shubh082002@gmail.com>
2025-05-02 12:35:04 +0530

    update session context

--- client/src/context/SessionContext.jsx
+++ client/src/context/SessionContext.jsx
@@ -1,15 +1,61 @@
 import { createContext, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { axiosInstance } from "../utils/axiosInstance";
 
 const SessionContext = createContext();
+
+const toDateString = (utcDate) => {
+  const date = new Date(utcDate);
+  const localDate = new Date(
+    date.getTime() + new Date().getTimezoneOffset() * -60000
+  );
+  return localDate.toISOString().split("T")[0]; // yyyy-mm-dd
+};
 
 export const SessionProvider = ({ children }) => {
   const [taskName, setTaskName] = useState("");
   const [selectedTaskId, setSelectedTaskId] = useState("");
+  const [sessions, setSessions] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(() =>
+    toDateString(new Date())
+  );
+  const [loading, setLoading] = useState(true);
+
+  const fetchSessions = async () => {
+    try {
+      const res = await axiosInstance.get("/focus-session");
+      const allSessions = res.data;
+
+      const filtered = allSessions.filter((session) => {
+        const localDateStr = toDateString(session.createdAt);
+        return localDateStr === selectedDate;
+      });
+
+      setSessions(filtered);
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to fetch session history");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   //console.log(selectedTaskId);
   return (
     <SessionContext.Provider
-      value={{ taskName, setTaskName, selectedTaskId, setSelectedTaskId }}
+      value={{
+        taskName,
+        setTaskName,
+        selectedTaskId,
+        setSelectedTaskId,
+        sessions,
+        setSessions,
+        selectedDate,
+        setSelectedDate,
+        loading,
+        setLoading,
+        fetchSessions,
+      }}
     >
       {children}
     </SessionContext.Provider>
