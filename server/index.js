ffacaa6c93e72d296671394309ce67a51d093e44
shubhamkv <shubh082002@gmail.com>
2025-05-03 21:51:30 +0530

    add otp route

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const { connectRedisServer } = require("./config/redisDB");
 require("dotenv").config();
 
 const userRouter = require("./routes/userRoutes");
 const taskRouter = require("./routes/taskRoutes");
 const focusSessionRouter = require("./routes/focusSessionRoutes");
+const otpRouter = require("./routes/otpHandler");
 
 const app = express();
 
 connectDB();
+connectRedisServer();
 
 app.use(cors());
 app.use(express.json());
@@ -17,5 +20,6 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/tasks", taskRouter);
 app.use("/api/focus-session", focusSessionRouter);
+app.use("/api/otp", otpRouter);
 
 app.listen(process.env.PORT);
