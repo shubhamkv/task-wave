e09f3882c5d73ce587272892eeba14d13f840e45
shubhamkv <shubh082002@gmail.com>
2025-05-05 22:51:45 +0530

    add theme preference to localStorage

--- client/src/context/ThemeContext.jsx
+++ client/src/context/ThemeContext.jsx
@@ -3,7 +3,9 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
@@ -13,6 +15,7 @@ export const ThemeProvider = ({ children }) => {
     //console.log(theme);
     if (theme === "dark") document.documentElement.classList.add("dark");
     else document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (
