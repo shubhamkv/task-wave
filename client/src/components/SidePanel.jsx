23aab0c583b63fd96e2e5db64029f5b5676a9885
shubhamkv <shubh082002@gmail.com>
2025-05-06 22:03:01 +0530

    add sidepanel

---
+++ client/src/components/SidePanel.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import sidePanelImage from "../assets/register.svg";
+
+export const SidePanel = () => {
+  return (
+    <div className="bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-400 text-white flex flex-col items-center justify-start p-10">
+      <div className="mt-20 mb-6 text-center px-4">
+        <h1 className="text-4xl text-indigo-600 md:text-5xl font-extrabold mb-3">
+          TaskWave
+        </h1>
+        <p className="text-lg text-gray-700 md:text-xl font-medium max-w-md leading-relaxed py-4">
+          "Small consistent actions lead to massive results. Stay focused and
+          ride the wave of productivity."
+        </p>
+      </div>
+      <img
+        src={sidePanelImage}
+        alt="Motivation"
+        className="rounded-xl shadow-lg w-3/4 max-w-sm mb-6"
+      />
+      <p className="text-medium text-center text-gray-800">
+        Want to return to the Home Page?{" "}
+        <Link
+          to="/"
+          className="text-blue-800 font-semibold hover:underline transition duration-150"
+        >
+          Click here
+        </Link>
+      </p>
+    </div>
+  );
+};
