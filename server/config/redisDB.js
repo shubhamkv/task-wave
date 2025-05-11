7ecc22cef234cfccd98183bfad7c54a0fb34f7f6
shubhamkv <shubh082002@gmail.com>
2025-05-11 23:14:44 +0530

    add redis connection string

--- server/config/redisDB.js
+++ server/config/redisDB.js
@@ -1,6 +1,14 @@
 const redis = require("redis");
+require("dotenv").config();
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    tls: false,
+  },
+  password: process.env.REDIS_PASSWORD,
+});
 
 const connectRedisServer = async () => {
   try {
